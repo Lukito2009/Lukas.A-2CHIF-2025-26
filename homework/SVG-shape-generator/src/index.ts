4bd85dfaf43ef3b5385591f3e19d0057ea557680
Lukas Auer <l.auer@students.htl-leonding.ac.at>
2025-09-26 09:13:54 +0200

    abgabe shape generator

--- homework/SVG-shape-generator/src/index.ts
+++ homework/SVG-shape-generator/src/index.ts
@@ -1,3 +1,45 @@
-const helloWorldHeader = document.getElementById("helloWorldHeader") as HTMLHeadingElement;
+const select = document.getElementById('ImgrementSelect') as HTMLSelectElement;
+const add = document.getElementById("add") as HTMLButtonElement;
 
-helloWorldHeader.innerText += "!";
+
+let value = "";
+add.addEventListener('click', () => {
+    value = select.value
+if (value === 'circle') {
+    const svg = document.getElementById("svg") as unknown as SVGCircleElement;
+    
+
+        const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle') as SVGCircleElement;
+
+        circle.setAttribute('fill', 'lime');
+        circle.setAttribute('stroke', 'green');
+        circle.setAttribute('cx', `${Math.random() * 200}`);
+        circle.setAttribute('cy', `${Math.random() * 200}`);
+        circle.setAttribute(`r`, `${Math.random() * 50}`);
+
+        svg.appendChild(circle);
+
+  
+
+} if (value === 'rect') {
+    const svg = document.getElementById("svg") as unknown as SVGRectElement;
+    
+
+        const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect') as SVGRectElement;
+
+        rect.setAttribute('fill', 'red');
+        rect.setAttribute('stroke', 'blue');
+        rect.setAttribute('x', `${Math.random() * 200}`);
+        rect.setAttribute('y', `${Math.random() * 200}`);
+        rect.setAttribute(`width`, `${20 + Math.random() * 100}`);
+        rect.setAttribute(`height`, `${20 + Math.random() * 50}`);
+
+        svg.appendChild(rect);
+
+
+
+}
+
+
+});
+
