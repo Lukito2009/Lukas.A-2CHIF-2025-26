80e5b766e9cf924d8e3e3c34e3e885635b6a63da
Lukas Auer <l.auer@students.htl-leonding.ac.at>
2025-10-03 11:03:47 +0200

    Add initial project structure and configuration files for SVG diagram generator

--- homework/SVG-shape-generator/src/index.ts
+++ homework/SVG-shape-generator/src/index.ts
@@ -1,45 +1,54 @@
 const select = document.getElementById('ImgrementSelect') as HTMLSelectElement;
 const add = document.getElementById("add") as HTMLButtonElement;
+const clear = document.getElementById("add") as HTMLButtonElement;
+
 
 
 let value = "";
+
+
+
+
 add.addEventListener('click', () => {
     value = select.value
-    if (value === 'circle') {
-        const svg = document.getElementById("svg") as unknown as SVGCircleElement;
+
+        if (value === 'circle') {
+            const svg = document.getElementById("svg") as unknown as SVGCircleElement;
 
 
-        const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle') as SVGCircleElement;
+            const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle') as SVGCircleElement;
 
-        circle.setAttribute('fill', 'lime');
-        circle.setAttribute('stroke', 'green');
-        circle.setAttribute('cx', `${Math.random() * 200}`);
-        circle.setAttribute('cy', `${Math.random() * 200}`);
-        circle.setAttribute(`r`, `${Math.random() * 50}`);
+            circle.setAttribute('fill', 'lime');
+            circle.setAttribute('stroke', 'green');
+            circle.setAttribute('cx', `${Math.random() * 200}`);
+            circle.setAttribute('cy', `${Math.random() * 200}`);
+            circle.setAttribute(`r`, `${Math.random() * 50}`);
 
-        svg.appendChild(circle);
-
-
-
-    } if (value === 'rect') {
-        const svg = document.getElementById("svg") as unknown as SVGRectElement;
-
-
-        const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect') as SVGRectElement;
-
-        rect.setAttribute('fill', 'red');
-        rect.setAttribute('stroke', 'blue');
-        rect.setAttribute('x', `${Math.random() * 200}`);
-        rect.setAttribute('y', `${Math.random() * 200}`);
-        rect.setAttribute(`width`, `${20 + Math.random() * 100}`);
-        rect.setAttribute(`height`, `${20 + Math.random() * 50}`);
-
-        svg.appendChild(rect);
+            svg.appendChild(circle);
 
 
 
-    }
+        } if (value === 'rect') {
+            const svg = document.getElementById("svg") as unknown as SVGRectElement;
 
 
+            const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect') as SVGRectElement;
+
+            rect.setAttribute('fill', 'red');
+            rect.setAttribute('stroke', 'blue');
+            rect.setAttribute('x', `${Math.random() * 200}`);
+            rect.setAttribute('y', `${Math.random() * 200}`);
+            rect.setAttribute(`width`, `${20 + Math.random() * 100}`);
+            rect.setAttribute(`height`, `${20 + Math.random() * 50}`);
+
+            svg.appendChild(rect);
+
+
+
+        }
+
+
+    
 });
+
 
